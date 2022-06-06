c81ad27ee672ddd4dbd6c8344d405bd50ec34059
rdnt42 <34737629+rdnt42@users.noreply.github.com>
2022-06-06 21:41:19 +0300

    Delete import.sql

--- src/main/resources/import.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO EQUIPMENT_STATES (id, name) VALUES (1, 'Доступен');
-INSERT INTO EQUIPMENT_STATES (id, name) VALUES (2, 'Недоступен');
-INSERT INTO EQUIPMENT_STATES (id, name) VALUES (3, 'Сбои в работе');
-
-INSERT INTO STANDS (stand_id, name, stand_status_id, metrics_status_id, dependent_session_status_id, independent_session_status_id) VALUES (1, 'B1', 3, 1, 2, 1);
-INSERT INTO STANDS (stand_id, name, stand_status_id, metrics_status_id, dependent_session_status_id, independent_session_status_id) VALUES (2, 'B3', 1, 1, 1, 1);
-INSERT INTO STANDS (stand_id, name, stand_status_id, metrics_status_id, dependent_session_status_id, independent_session_status_id) VALUES (3, 'B5', 3, 2, 1, 1);
-INSERT INTO STANDS (stand_id, name, stand_status_id, metrics_status_id, dependent_session_status_id, independent_session_status_id) VALUES (4, 'SI1', 2, 2, 2, 2);
-INSERT INTO STANDS (stand_id, name, stand_status_id, metrics_status_id, dependent_session_status_id, independent_session_status_id) VALUES (5, 'SI3', 1, 1, 1, 1);
